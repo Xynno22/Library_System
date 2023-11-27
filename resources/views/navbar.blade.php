beded5ade4c3da272f588a73e6bea02df83450c2
Xynno22 <njackson2208@gmail.com>
2023-11-27 19:44:34 +0700

    ,

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Navbar</title>
+    <link rel="stylesheet" href="resources/css/app.css">
 </head>
 <body>
     <nav>
